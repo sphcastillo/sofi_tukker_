00a2ec89877f5a7ff37d1fb864c031ce7ff3144a
sphcastillo <sphcastillo@gmail.com>
2024-09-04 20:49:35 -0700

    Incorporating the Sofi Tukker storeFront

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,4 +1,7 @@
+import SofiTukkerStore from "@/components/SofiTukkerStore";
+import SofiTukkerStoreLogo from "@/images/storeFrontpage/sofitukkerstorelogo.png";
 import localFont from "next/font/local";
+import Image from "next/image";
 
 const franklinGothicCondensed = localFont({
   src: "../../public/fonts/FGC.ttf",
@@ -7,9 +10,20 @@ const franklinGothicCondensed = localFont({
 export default function StorePage
 () {
   return (
-    <div className="pt-12">
-      <div className={`${franklinGothicCondensed.className} flex items-center justify-center `}>
-        <span className="text-[28px]">HEY HOMIE, COMING SOON! ...</span>
+    <div className="w-full bg-[#FFF3E8] pb-[124px]">
+      <div className="flex items-center justify-center pt-7">
+        <Image 
+          src={SofiTukkerStoreLogo}
+          alt="Sofi Tukker Store Logo"
+          width={800}
+          height={225}
+          className=""
+          priority
+        />
+      </div>
+
+     <div className="pt-12">
+      <SofiTukkerStore />
       </div>
     </div>
   )
---
+++ components/SofiTukkerStore.tsx
@@ -0,0 +1,161 @@
+import Image, { StaticImageData } from "next/image";
+import Vinyl from "@/images/musicStore/vinyl.png";
+import CD from "@/images/musicStore/cd.png";
+import Cassette from "@/images/musicStore/cassette.png";
+import hoodie from "@/images/breadMerchandise/breadHoodie1.png";
+import breadtshirt from "@/images/breadMerchandise/breadTee2.png";
+import crop from "@/images/breadMerchandise/breadCrop3.png";
+import joggers from "@/images/breadMerchandise/breadJoggers4.png";
+import tshirt from "@/images/breadMerchandise/breadTee5.png";
+import thong from "@/images/breadMerchandise/breadThong6.png";
+import socks from "@/images/breadMerchandise/breadSocks7.png";
+import tee from "@/images/breadMerchandise/breadTee8.png";
+import sticker from "@/images/breadMerchandise/breadSticker9.png";
+import st1 from "@/images/storeFrontpage/stMusic1.png";
+import st2 from "@/images/storeFrontpage/stMusic2.png";
+import st3 from "@/images/storeFrontpage/stMusic3.png";
+import st4 from "@/images/storeFrontpage/stMusic4.png";
+import st5 from "@/images/storeFrontpage/stMusic5.png";
+import st6 from "@/images/storeFrontpage/stMusic6.png";
+import st7 from "@/images/storeFrontpage/stMusic7.png";
+
+type BandStoreFrontItems = {
+  id: number;
+  title: string;
+  image: StaticImageData;
+  price: string;
+};
+
+const BandStoreProducts: BandStoreFrontItems[] = [
+  {
+    id: 1,
+    title: "VINYL",
+    price: "34.99",
+    image: Vinyl,
+  },
+  {
+    id: 2,
+    title: "CD",
+    price: "14.99",
+    image: CD,
+  },
+  {
+    id: 3,
+    title: "CASSETTE",
+    price: "14.99",
+    image: Cassette,
+  },
+  {
+    id: 4,
+    title: "HOODIE",
+    price: "125.00",
+    image: hoodie,
+  },
+  {
+    id: 5,
+    title: "TEE",
+    price: "45.00",
+    image: breadtshirt,
+  },
+  {
+    id: 6,
+    title: "CROP",
+    price: "35.00",
+    image: crop,
+  },
+  {
+    id: 7,
+    title: "JOGGERS",
+    price: "110.00",
+    image: joggers,
+  },
+  {
+    id: 8,
+    title: "TEE",
+    price: "45.00",
+    image: tshirt,
+  },
+  {
+    id: 9,
+    title: "THONG",
+    price: "35.00",
+    image: thong,
+  },
+  {
+    id: 10,
+    title: "SOCKS",
+    price: "30.00",
+    image: socks,
+  },
+  {
+    id: 11,
+    title: "TEE",
+    price: "45.00",
+    image: tee,
+  },
+  {
+    id: 12,
+    title: "STICKER",
+    price: "6.00",
+    image: sticker,
+  },
+  {
+    id: 13,
+    title: 'SOFT ANIMALS LIMITED EDITION GREEN VINYL',
+    price: '35.00',
+    image: st1
+  },
+  {
+    id: 14,
+    image: st2,
+    title: "WET TENNIS CD",
+    price: '14.99'
+  },
+  {
+    id: 15,
+    image: st3,
+    title: "WET TENNIS VINYL",
+    price: "29.99"
+  },
+  {
+    id: 16,
+    image: st4,
+    title: "TREEHOUSE VINYL",
+    price: "29.99"
+  },
+  {
+    id: 17,
+    image: st5,
+    title: "SOFT ANIMALS CD",
+    price: "12.00"
+  },
+  {
+    id: 18,
+    image: st6,
+    title: "DANCING ON THE PEOPLE VINYL",
+    price: "24.99"
+  },
+  {
+    id: 19,
+    image: st7,
+    title: "SOFT ANIMALS VINYL",
+    price: "24.99"
+  }
+
+];
+
+export default function SofiTukkerStore() {
+  return (
+    <div className="bg-[#FFF3E8]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {BandStoreProducts.map((item) => (
+          <div key={item.id} className="flex flex-col items-center">
+            <Image src={item.image} alt={item.title} />
+            <h3>{item.title}</h3>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
