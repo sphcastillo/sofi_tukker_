232a9cfb699fef63653df0c9b404981d7fcd6a61
sphcastillo <sphcastillo@gmail.com>
2025-02-05 13:04:44 -0800

    Tour page: additional tour dates added

--- app/tour/page.tsx
+++ app/tour/page.tsx
@@ -5,111 +5,291 @@ const franklinGothicCondensed = localFont({
   src: "../../public/fonts/FGC.ttf",
 });
 
+// {
+//   date: "",
+//   venue: "",
+//   city: "",
+//   VIP: false,
+//   tickets: false,
+// },
+
 const tourDates = [
-  // { date: "SEP 18, 2024", venue: "MOODY AMPHITHEATER", city: "AUSTIN, TX" },
-  // { date: "SEP 20, 20024", venue: "BAYOU MUSIC CENTER", city: "HOUSTON, TX" },
-  // { date: "SEP 21, 2024", venue: "SOUTH SIDE BALLROOM", city: "DALLAS, TX" },
-  // { date: "SEP 24, 2024", venue: "TABERNACLE", city: "ATLANTA, GA" },
-  // { date: "SEP 26, 2024", venue: "FOREST HILLS STADIUM", city: "QUEENS, NY" },
-  // { date: "SEP 30, 2024", venue: "AGORA THEATRE AND BALLROOM", city: "CLEVELAND, OH" },
-  // { date: "OCT 2, 2024", venue: "FRANKLIN MUSIC HALL", city: "PHILADELPHIA, PA" },
-  // { date: "OCT 3, 2024", venue: "ROADRUNNER", city: "BOSTON, MA" },
-  // { date: "OCT 4, 2024", venue: "ROADRUNNER", city: "BOSTON, MA" },
-  // { date: "OCT 5, 2024", venue: "THE ANTHEM", city: "WASHINGTON, DC" },
-  // { date: "OCT 8, 2024", venue: "MTELUS", city: "MONTREAL, QC" },
-  // { date: "OCT 9, 2024", venue: "MTELUS", city: "MONTREAL, QC" },
-  // { date: "OCT 11, 2024", venue: "HISTORY", city: "TORONTO, ON" },
-  // { date: "OCT 12, 2024", venue: "HISTORY", city: "TORONTO, ON" },
-  // { date: "OCT 18, 2024", venue: "THE ARMORY", city: "MINNEAPOLIS, MN" },
-  // { date: "OCT 19, 2024", venue: "BYLINE BANK ARAGON BALLROOM", city: "CHICAGO, IL" },
-  // { date: "OCT 22, 2024", venue: "THE MIDLAND THEATRE", city: "KANSAS CITY, MO" },
-  // { date: "OCT 24, 2024", venue: "RED ROCKS AMPHITHEATRE", city: "MORRISON, CO" },
-  // { date: "OCT 25, 2024", venue: "ROCKWELL AT THE COMPLEX", city: "SALT LAKE CITY, UT" },
-  // { date: "OCT 29, 2024", venue: "THEATER OF THE CLOUDS AT MODA CENTER", city: "PORTLAND, OR" },
-  // { date: "OCT 30, 2024", venue: "WAMU THEATER", city: "SEATTLE, WA" },
-  // { date: "NOV 2, 2024", venue: "THE TORCH AT THE LA MEMORIAL COLISEUM", city: "LOS ANGELES, CA" },
-  // { date: "NOV 3, 2024", venue: "GALLAGHER SQUARE AT PETCO PARK", city: "SAN DIEGO, CA" },
-  // { date: "NOV 7, 2024", venue: "SANTA BARBARA BOWL", city: "SANTA BARBARA, CA" },
-  // { date: "NOV 8, 2024", venue: "BILL GRAHAM CIVIC AUDITORIUM", city: "SAN FRANCISCO, CA" },
-  // { date: "NOV 15, 2024", venue: "MARQUEE NIGHTCLUB [DJ RESIDENCY]", city: "LAS VEGAS, NV" },
-  // { date: "NOV 20, 2024", venue: "GROSSE FREIHEIT 36", city: "HAMBURG, GERMANY" },
-  // { date: "NOV 21, 2024", venue: "LANXESS ARENA", city: "COLOGNE, GERMANY" },
-  // { date: "NOV 23, 2024", venue: "WIENER STADTHALLE", city: "VIENNA, AUSTRIA" },
-  // { date: "NOV 24, 2024", venue: "UBER EATS MUSIC HALL", city: "BERLIN, GERMANY" },
-  // { date: "NOV 26, 2024", venue: "ZIGGO DOME", city: "AMSTERDAM, NETHERLANDS" },
-  // { date: "NOV 29, 2024", venue: "MEDIOLANUM FORUM", city: "MILAN, ITALY" },
-  // { date: "DEC 1, 2024", venue: "KOMPLEX 457", city: "ZÜRICH, SWITZERLAND" },
-  // { date: "DEC 3, 2024", venue: "WIZINK CENTER", city: "MADRID, SPAIN" },
-  // { date: "DEC 6, 2024", venue: "ING ARENA", city: "BRUSSELS, BELGIUM" },
-  // { date: "DEC 7, 2024", venue: "ACCOR ARENA", city: "PARIS, FRANCE" },
-  // { date: "DEC 11, 2024", venue: "THE 02", city: "LONDON, UK", VIP: false, tickets: true },
-  // { date: "DEC 12, 2024", venue: "NIGHT TALES (DJ SET)", city: "LONDON, UK", VIP: false, tickets: false },
-  // { date: "DEC 13, 2024", venue: "3ARENA", city: "DUBLIN, IRELAND", VIP: false, tickets: true },
-  { date: 'FEB 22, 2025', venue: 'PALM TREE MUSIC FESTIVAL ASPEN (DJ SET)', city: 'ASPEN, CO', VIP: false, tickets: true },
-  { date: 'MAR 26, 2025 - MAR 30, 2025', venue: 'TREEFORT MUSIC FEST (DJ SET)', city: 'BOISE, ID', VIP: false, tickets: true },
-  { date: 'JUL 16, 2025 - JUL 19, 2025', venue: 'COLOURS OF OSTRAVA', city: 'OSTRAVA, MORAVIAN-SILESIAN REGION', VIP: false, tickets: true },
+  {
+    date: "FEB 20, 2025",
+    venue: "BELLY UP ASPEN (DJ SET)",
+    city: "ASPEN, CO",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "FEB 21, 2025",
+    venue: "THE SNOW LODGE (DJ SET)",
+    city: "ASPEN, CO",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "FEB 22, 2025",
+    venue: "PALM TREE MUSIC FESTIVAL ASPEN (DJ SET)",
+    city: "ASPEN, CO",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAR 6, 2025",
+    venue: "AT OF THE WILD (DJ SET)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAR 28, 2025",
+    venue: 'ENCORE BEACH CLUB (DJ RESIDENCY)',
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAR 29, 2025",
+    venue: "TREEFORT MUSIC FEST (DJ SET)",
+    city: "BOISE, ID",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "APR 5, 2025",
+    venue: "PALM TREE MUSIC FESTIVAL (DJ SET)",
+    city: "MIAMI, FL",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "APR 5, 2025",
+    venue: "LIV (DJ SET)",
+    city: "MIAMI BEACH, FL",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "APR 18, 2025 - APR 19, 2025",
+    venue: "BREAKAWAY MUSIC FESTIVAL ARIZONA (DJ SET)",
+    city: "PHOENIX, AZ",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "APR 26, 2025",
+    venue: "BIG NIGHT LIVE",
+    city: "BOSTON, MA",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAY 2, 2025",
+    venue: "ENCORE BEACH CLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAY 23, 2025 - MAY 25, 2025",
+    venue: "BOTTLE ROCK NAPA VALLEY",
+    city: "NAPA, CA",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "MAY 31, 2025",
+    venue: "SILO",
+    city: "DALLAS, TX",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUN 13, 2025",
+    venue: "XS NIGHTCLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUN 25, 2025",
+    venue: "ENCORE BEACH CLUB AT NIGHT (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUN 27, 2025",
+    venue: "XS NIGHTCLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUL 15, 2025",
+    venue: "XS NIGHTCLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUL 16, 2025 - JUL 20, 2025",
+    venue: "ELECTRIC CASTLE",
+    city: "TRANSYLVANIA, ROMANIA",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUL 19, 2025",
+    venue: "COLOURS OF OSTRAVA",
+    city: "OSTRAVA, MORAVIAN-SILESIAN REGION",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "JUL 20, 2025",
+    venue: "TOMORROWLAND (DJ SET)",
+    city: "BOOM, ANTWERP, BELGIUM",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "AUG 8, 2025 - AUG 10, 2025",
+    venue: "ELEMENTS MUSIC & ARTS FESTIVAL",
+    city: "LONG POND, PA",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "AUG 9, 2025 - AUG 10, 2025",
+    venue: "ÎLESONIQ FESTIVAL",
+    city: "MONTREAL, CANADA",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "AUG 13, 2025",
+    venue: "ENCORE BEACH CLUB AT NIGHT (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "AUG 15, 2025",
+    venue: "XS NIGHTCLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "AUG 29, 2025",
+    venue: "ENCORE BEACH CLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "SEP 14, 2025",
+    venue: "ENCORE BEACH CLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
+  {
+    date: "SEP 26, 2025",
+    venue: "XS NIGHTCLUB (DJ RESIDENCY)",
+    city: "LAS VEGAS, NV",
+    VIP: false,
+    tickets: true,
+  },
 ];
-
-
 
 export default function TourPage() {
   return (
     <div className="pt-12 pb-5 bg-[#b9ddba]">
       <div className="flex items-center justify-center pb-8">
-        <Image 
-          src={TourTitle} 
-          alt="Sofi Tukker - Tour title" 
+        <Image
+          src={TourTitle}
+          alt="Sofi Tukker - Tour title"
           priority
           className="w-[270px] xxs:w-[312px] xs:w-[412px] sm:w-[487px] md:w-[432px] lg:w-[600px]"
         />
       </div>
-      
+
       <div className="container mx-auto p-4">
-      {tourDates.map((tour, index) => (
-        <div key={index} className="grid grid-cols-3 gap-4 items-center bg-[#b9ddba] p-1 xxs:p-2 xs:p-4 md:px-12 lg:px-[120px] rounded-lg mb-4 hover:bg-[#fa264e] transition-colors duration-300">
-          {/* First Column: Date and Venue */}
-          <div>
-            <div className={`${franklinGothicCondensed.className} text-black text-[15px] xxs:text-[18px] sm:text-[22px]`}>{tour.date}</div>
-            <div className={`${franklinGothicCondensed.className} text-black text-[15px] xxs:text-[18px] sm:text-[22px]`}>{tour.venue}</div>
+        {tourDates.map((tour, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-3 gap-4 items-center bg-[#b9ddba] p-1 xxs:p-2 xs:p-4 md:px-12 lg:px-[120px] rounded-lg mb-4 hover:bg-[#fa264e] transition-colors duration-300"
+          >
+            {/* First Column: Date and Venue */}
+            <div>
+              <div
+                className={`${franklinGothicCondensed.className} text-black text-[15px] xxs:text-[18px] sm:text-[22px]`}
+              >
+                {tour.date}
+              </div>
+              <div
+                className={`${franklinGothicCondensed.className} text-black text-[15px] xxs:text-[18px] sm:text-[22px]`}
+              >
+                {tour.venue}
+              </div>
+            </div>
+
+            {/* Second Column: City */}
+            <div
+              className={`${franklinGothicCondensed.className} text-center text-[15px] xxs:text-[18px] sm:text-[22px] text-black`}
+            >
+              {tour.city}
+            </div>
+
+            {/* Third Column: Buttons */}
+            <div className="flex justify-end gap-2 flex-col xs:flex-row">
+              {tour.VIP && (
+                <button
+                  className={`${franklinGothicCondensed.className} w-full text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}
+                >
+                  VIP
+                </button>
+              )}
+              {tour.tickets ? (
+                <button
+                  className={`${franklinGothicCondensed.className} w-full ${
+                    !tour.VIP ? "xl:max-w-[240px]" : ""
+                  } text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}
+                >
+                  Tickets
+                </button>
+              ) : (
+                <button
+                  className={`${franklinGothicCondensed.className} w-full ${
+                    !tour.VIP ? "xl:max-w-[240px]" : ""
+                  } text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}
+                >
+                  Join Waitlist
+                </button>
+              )}
+            </div>
           </div>
+        ))}
+      </div>
 
-          {/* Second Column: City */}
-          <div className={`${franklinGothicCondensed.className} text-center text-[15px] xxs:text-[18px] sm:text-[22px] text-black`}>{tour.city}</div>
+      <div className="flex items-center justify-center pb-4 md:pt-10">
+        <span
+          className={`${franklinGothicCondensed.className} text-[15px] xxs:text-[18px] sm:text-[22px] text-center`}
+        >
+          GET NOTIFIED WHEN NEW EVENTS ARE ANNOUNCED IN YOUR AREA
+        </span>
+      </div>
 
-          {/* Third Column: Buttons */}
-          <div className="flex justify-end gap-2 flex-col xs:flex-row">
-            {tour.VIP && (
-            <button className={`${franklinGothicCondensed.className} w-full text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}>
-              VIP
-            </button>
-          )}
-          {tour.tickets ? (
-            <button className={`${franklinGothicCondensed.className} w-full ${
-              !tour.VIP ? 'xl:max-w-[240px]' : ''
-            } text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}>
-              Tickets
-            </button>
-          ) : (
-            <button className={`${franklinGothicCondensed.className} w-full ${
-              !tour.VIP ? 'xl:max-w-[240px]' : ''
-            } text-[#dd90e1] bg-[#000000] text-[15px] xxs:text-[18px] sm:text-[22px] px-2 py-2 xs:px-4 rounded-xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}>
-              Join Waitlist
-            </button>
-          )}
-          </div>
-        </div>
-      ))}
+      <div className="flex items-center justify-center pb-[72px]">
+        <button
+          className={`${franklinGothicCondensed.className} text-[15px] xxs:text-[18px] sm:text-[22px] text-[#dd90e1] bg-[#000000] py-2 px-4 rounded-3xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}
+        >
+          FOLLOW SOFI TUKKER
+        </button>
+      </div>
     </div>
-
-    <div className="flex items-center justify-center pb-4 md:pt-10">
-      <span className={`${franklinGothicCondensed.className} text-[15px] xxs:text-[18px] sm:text-[22px] text-center`}>GET NOTIFIED WHEN NEW EVENTS ARE ANNOUNCED IN YOUR AREA</span>
-    </div>
-
-    <div className="flex items-center justify-center pb-[72px]">
-      <button className={`${franklinGothicCondensed.className} text-[15px] xxs:text-[18px] sm:text-[22px] text-[#dd90e1] bg-[#000000] py-2 px-4 rounded-3xl hover:text-[#fa264e] hover:bg-[#dd90e1] hover:border-2 hover:border-white`}>
-        FOLLOW SOFI TUKKER
-      </button>
-    </div>
-    </div>
-  )
+  );
 }
